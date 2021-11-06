fb895002e0d7191bca986bf917c783fdcba0fbdb
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-11-05 23:58:39 -0300

    Retirando code desnecessario

--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
     require_once '../vendor/autoload.php';
     $route = new \App\Route;
-
-    print_r($route->getUrl());
-
-     echo "<br> Estamos acessando o index";
+    
+    //Aqui é a index geral
 ?>
