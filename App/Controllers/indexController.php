9124f7e961ed16015ca155c723bea7528c630aa9
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-11-06 01:47:09 -0300

    Criando metodos de renderizar viewers e render view (passando dados também)

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -3,12 +3,27 @@
   
     {
     class IndexController{
+            private $view;
+
+            public function __construct(){
+                $this->view = new \stdClass();
+            }
+
             public function index(){
-                echo"Chegamos a index controller e disparamos o index"; 
+                $this->view->dados = ['Samuel', 'Professor', 'Facilitador'];
+                $this->render('index');
             }
 
             public function sobreNos(){
-                echo"Chegamos a index controller e disparamos Sobre nos"; 
+                $this->view->dados = ['Bonito', 'Diversificado', 'Especial'];
+                $this->render('sobreNos');
+            }
+
+            public function render($view){
+                $classAtual = get_class($this);
+                $classAtual = str_replace('App\\Controllers\\', '', $classAtual);
+                $classAtual = strtolower(str_replace('Controller', '', $classAtual));
+                require_once '../App/Views/'.$classAtual.'/'.$view.'.phtml';
             }
         } 
     }
