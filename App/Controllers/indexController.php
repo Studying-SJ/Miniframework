a39ca9b856fcc1987b3ffb26a35c88141c6d6f43
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-11-05 23:51:31 -0300

    Criando controle de paginas

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -1,3 +1,16 @@
 <?php
+  namespace App\Controllers
+  
+    {
+    class IndexController{
+            public function index(){
+                echo"Chegamos a index controller e disparamos o index"; 
+            }
+
+            public function sobreNos(){
+                echo"Chegamos a index controller e disparamos Sobre nos"; 
+            }
+        } 
+    }
     
 ?>
