92bb58c22360c876d8cb926d568258a4e89bac4e
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-11-08 02:22:25 -0300

    Adicionando get com Model de produto e estabelecendo conexao

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -2,13 +2,23 @@
   namespace App\Controllers;
 
   use MF\Controller\Action;
+  use App\Connection;
+  use App\Models\Produto;
+
 
   
     {
     class IndexController extends Action{
 
             public function index(){
-                $this->view->dados = ['Samuel', 'Professor', 'Facilitador'];
+                //$this->view->dados = ['Samuel', 'Professor', 'Facilitador'];
+
+               $conn = Connection::getDb();
+               $produto = new Produto($conn);
+               $produtos = $produto->getProdutos();
+
+               $this->view->dados = $produtos;
+
                 $this->render('index', 'layout');
             }
 
