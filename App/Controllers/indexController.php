82b46659cd87cb8180df36c594bb9e5d61283661
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-11-09 20:58:58 -0300

    Refatorando e facilitando a conexao usando a camada Container para obter models e conexao

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -2,8 +2,9 @@
   namespace App\Controllers;
 
   use MF\Controller\Action;
-  use App\Connection;
+  use Mf\Model\Container;
   use App\Models\Produto;
+  use App\Models\Info;
 
 
   
@@ -11,19 +12,20 @@
     class IndexController extends Action{
 
             public function index(){
-                //$this->view->dados = ['Samuel', 'Professor', 'Facilitador'];
 
-               $conn = Connection::getDb();
-               $produto = new Produto($conn);
-               $produtos = $produto->getProdutos();
+                $produto = Container::getModel('Produto');
+                $produtos = $produto->getProdutos();
 
-               $this->view->dados = $produtos;
-
+                $this->view->dados = $produtos;
                 $this->render('index', 'layout');
             }
 
             public function sobreNos(){
-                $this->view->dados = ['Bonito', 'Diversificado', 'Especial'];
+
+                $info = Container::getModel('Info');
+                $informacoes = $info->getInfo();
+                
+                $this->view->dados = $informacoes;
                 $this->render('sobreNos', 'layout2');
             }
 
